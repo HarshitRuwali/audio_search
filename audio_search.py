95bb56ebff0f612a3fda40cd4c94547a3f1105f2
harshitruwali <harshitruwali@outlook.com>
2019-09-30 22:40:53 +0530

    Add files via upload

--- audio_search.py
+++ audio_search.py
@@ -3,7 +3,6 @@ import speech_recognition as sr
 r=sr.Recognizer()
 
 t=0
-f = open("output.txt", "a")
 
 with sr.Microphone() as source:
 	print('Speak English :')
@@ -12,16 +11,14 @@ with sr.Microphone() as source:
 		audio =r.listen(source)
 		try:
 			text =r.recognize_google(audio)
-			print('you said :{}'.format(text), file=open("output.txt", "a"))
 			print('you said :{}'.format(text))
 			t=1
 
 		except:
-			print('Not understable')
+			print('Not understandable')
 			print('Try again')
 			t==0
 
-f.close()
 
 
 from googlesearch import *
